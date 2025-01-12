181c6bc7a4ec7991c5ab3e77b7c8becd9b667541
si989 <sickstyle095@gmail.com>
2025-01-12 12:04:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "28387710"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "659e4c6ca523efe415dda368002ec993"")
+API_HASH = os.environ.get("API_HASH", "659e4c6ca523efe415dda368002ec993")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "6104143606"))
